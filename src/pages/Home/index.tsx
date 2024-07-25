86e3ff4c4a905a8389cf81391ae1184ce35dce82
unknown <allan.rodrigues14@hotmail.com>
2024-07-24 22:10:27 -0300

    #Production - Page Home and css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,31 @@
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+
 export function Home() {
     return (
-        <div>
+        <HomeContainer>
             <form action="">
-                <label htmlFor="task">Vou trabalhar em</label>
-                <input id="task" type="text" />
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <input id="task" type="text" />
 
-                <label htmlFor="minutesAmount"></label>
-                <input id="minutesAmount" type="number" />
+                    <label htmlFor="minutesAmount"></label>
+                    <input id="minutesAmount" type="number" />
 
-                <span>minutos.</span>
+                    <span>minutos.</span>
+                </FormContainer>
+
+
+                <CountDownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountDownContainer>
+
+                <button type="submit"><Play size={24} /> Começar</button>
             </form>
-        </div>
+        </HomeContainer>
     )
 }
