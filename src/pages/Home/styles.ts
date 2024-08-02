47b418db82ff470981c5afc11b7fd676ca9a42a7
unknown <allan.rodrigues14@hotmail.com>
2024-08-01 22:02:21 -0300

    Customize input task and Start countDown

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -38,10 +38,9 @@ export const CountDownContainer = styled.div`
     span {
         background: ${(props) => props.theme['gray-700']};
         padding: 2rem 1rem;
-        border: 8px;
+        border-radius: 8px;
     }
 `;
-
 
 export const Separator = styled.div`
     padding: 2rem 0;
@@ -50,4 +49,59 @@ export const Separator = styled.div`
     overflow: hidden;
     display: flex;
     justify-content: center;
+`;
+
+const BaseInput = styled.input`
+    background: transparent;
+    border: 0;
+    height: 2.5rem;
+    border-bottom:  2px solid ${(props) => props.theme['gray-500']};
+    font-weight: bold;
+    font-size: 1.025rem;
+    padding: 0 0.5rem;
+    color: ${(props) => props.theme['gray-100']};
+    &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+    }
+
+    &:focus {
+        box-shadow: none;
+        border-color: ${(props) => props.theme['green-500']};
+    }
+`;
+
+export const TaskInput = styled(BaseInput)`
+    flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+    width: 4rem;
+`;
+
+export const StartCountdownButton = styled.button`
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    font-weight: bold;
+    cursor: pointer;
+    margin-top: 1rem;
+
+    background: ${(props) => props.theme['green-500']};
+    color: ${(props) => props.theme['green-300']};
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    &:not(:disabled)hover {
+        background: ${(props) => props.theme['green-700']};
+    }
 `;
